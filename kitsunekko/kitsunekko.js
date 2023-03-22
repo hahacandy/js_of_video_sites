f5aeae20f128921381bd9b1da20448edc095e8ea
hahacandy <58638224+hahacandy@users.noreply.github.com>
2023-03-22 22:26:48 +0900

    랜덤애니메 버튼 추가

--- kitsunekko/kitsunekko.js
+++ kitsunekko/kitsunekko.js
@@ -1,13 +1,26 @@
 //https://kitsunekko.net/dirlist.php?dir=subtitles%2Fjapanese%2F
 
-animeList = document.getElementsByTagName('tr')
 var index = 0;
 
-function serachAnime(index, animeName){
-	window.open('https://www.google.com/search?q=Anime '+ animeName + '&tbm=isch', "_top ");
+function serachAnime(index){
+	var temp = document.getElementById('anime_' + index);
+	var animeName = temp.childNodes[0].textContent.trim()
+	
+	window.open('https://www.google.com/search?q=Anime "'+ animeName + '"&tbm=isch', "_top ");
+}
+
+function searchRandomAnime(){
+	var randomIndex = Math.floor(Math.random() * index);
+	serachAnime(randomIndex);
 }
 
 if(document.querySelector('#breadcrumbs').textContent.includes(' kitsunekko.net \n > Japanese subtitles > ') == false){
+	
+	//create search button
+	
+	var animeList = document.getElementsByTagName('tr')
+	
+	
 	for(var anime of animeList){
 		anime.setAttribute('id', 'anime_'+index);
 		
@@ -18,10 +31,32 @@ if(document.querySelector('#breadcrumbs').textContent.includes(' kitsunekko.net 
 		animeName = animeName.replace(regex, "");
 	
 		var addTag = document.createElement("button");
-		addTag.setAttribute('onclick', 'serachAnime('+index+',"' + animeName + '")');
+		addTag.setAttribute('onclick', 'serachAnime('+index+')');
 		addTag.innerHTML = "Search";
 		anime.append(addTag);
 		
 		index = index +1;
 	}
+	
+	
+	//create random button
+	
+	var temp = document.querySelector('#flisttable > tbody');
+	
+	var addTag = document.createElement("tr");
+	
+	
+	var addTag2 = document.createElement("td");
+	addTag2.style.textAlign = "center";
+	addTag2.setAttribute('colspan', 3);
+	
+	var addTag3 = document.createElement("button");
+		addTag3.setAttribute('onclick', 'searchRandomAnime()');
+		addTag3.innerHTML = "Search Random Anime";
+		
+		addTag2.appendChild(addTag3)
+	
+	addTag.appendChild(addTag2);
+	
+	temp.prepend(addTag);
 }
